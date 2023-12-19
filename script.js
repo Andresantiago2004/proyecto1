f7f3ca9f8087a1bc40b5e9a36159c13691d6c69d
Programador <137370610+Andresantiago2004@users.noreply.github.com>
2023-12-18 19:06:12 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function redireccion(){
+    location.href = "preguntas.html"
+}
+
+
+const loginr = document.querySelector('.login-section')
+const loginlink = document.querySelector('.login-link')
+const registerlink = document.querySelector('.register-link')
+registerlink.addEventListener('click',()=>{
+    loginr.classList.add('active')
+})
+loginlink.addEventListener('click',()=>{
+    loginr.classList.remove('active')
+})
